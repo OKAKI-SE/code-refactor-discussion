67756c08194508d677c5a2cec04c8fa57faeab14
OKAKI-SE <47049390+OKAKI-SE@users.noreply.github.com>
2019-12-28 17:09:20 +0900

    Update calc.py

--- basic/calc/calc.py
+++ basic/calc/calc.py
@@ -1,15 +1,19 @@
-""" 2つの値の足し算と引き算の結果を表示する. """
+"""
+2つの値の足し算と引き算の結果を表示する
+"""
 
-
-def calc(num1, num2):
+def calc_add(num1, num2):
     result_add = num1 + num2
-    result_sub = num1 - num2
+    return result_add
 
-    return result_add, result_sub
+def calc_sub(num1, num2):
+    result_sub = num1 - num2
+    return result_sub
 
 
 if __name__ == "__main__":
-    add, sub = calc(10, 5)
+    add = calc_add(10, 5)
+    sub = calc_sub(10, 5)
 
-    print(f"{add=}")
-    print(f"{sub=}")
+    print(f"{add}")
+    print(f"{sub}")
